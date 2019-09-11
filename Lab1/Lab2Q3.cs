cef92514859c90b212bd6844ba31a8c3a51d7066
AsmitaChandrakar <54538106+AsmitaChandrakar@users.noreply.github.com>
2019-09-11 23:49:54 +0530

    Add files via upload

---
+++ Lab1/Lab2Q3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2Q3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] arr = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter the name of city");
+                arr[i] = Console.ReadLine();
+            }
+
+            foreach (string i in arr)
+            {
+                Console.WriteLine(i);
+
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
